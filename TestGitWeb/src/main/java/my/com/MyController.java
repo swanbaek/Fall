ca120f9318145d11c5b622f842edab10d9572c1e
iibi0108 <iibi0108@iibi0108-PC>
2017-08-21 11:09:04 +0900

    MyController추가

---
+++ TestGitWeb/src/main/java/my/com/MyController.java
@@ -0,0 +1,14 @@
+package my.com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+	@RequestMapping("/testGit")
+	public String testGit(){
+		return "hello";
+	}
+	
+}
